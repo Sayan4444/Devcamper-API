cbfbb10234562c35ab66ebff0aab65cd7f52a685
Sayan Banerjee <sayanbanerjee2002@gmail.com>
2024-12-23 15:48:34 +0530

    utils/errorResponse converted to ts

--- src/utils/errorResponse.ts
+++ src/utils/errorResponse.ts
@@ -1,7 +1,7 @@
-class ErrorResponse extends Error {
+export default class ErrorResponse extends Error {
+    public statusCode: number;
     constructor(message, statusCode) {
         super(message);
         this.statusCode = statusCode;
     }
 }
-module.exports = ErrorResponse;
